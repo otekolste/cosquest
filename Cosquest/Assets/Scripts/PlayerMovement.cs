494679aa1b9b7db201c883832babec8a0add5b47
oliviatek <58699635+oliviatek@users.noreply.github.com>
2023-02-13 20:48:28 -0500

    tweaked movement script, added character animation controllers, added new tile assets

---
+++ Cosquest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// code adapted from here: https://github.com/Brackeys/2D-Animation/blob/master/2D%20Animation/Assets/Scripts/PlayerMovement.cs
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public PlayerController controller;
+	public Animator animator;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+
+			animator.SetBool("IsJumping", true);
+		}
+
+
+
+	}
+
+	public void OnLanding()
+	{
+		animator.SetBool("IsJumping", false);
+		Debug.Log("landed!");
+	}
+
+
+
+	void FixedUpdate()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+		jump = false;
+	}
+}
