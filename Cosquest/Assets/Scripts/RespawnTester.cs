ee9c0c0e859a867cdbe89b52bbe6be176c20fef2
Rachel Park-Lin <89550184+ZachariahDawood@users.noreply.github.com>
2023-02-27 15:18:20 -0500

    checkpoints!!
    added a checkpointcontroller object that controls the checkpoints and a checkpoint object that sets where they should be (and a component to the wanderer that resets their position when the backspace key is hit)

---
+++ Cosquest/Assets/Scripts/RespawnTester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnTester : MonoBehaviour
+{
+    private CheckpointController cpc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cpc = GameObject.FindGameObjectWithTag("CPC").GetComponent<CheckpointController>();
+        transform.position = cpc.lastCheckpoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
