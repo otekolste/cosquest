de2fdc2e424bef286369ecc5209fff568ad00383
Rachel Park-Lin <89550184+ZachariahDawood@users.noreply.github.com>
2023-04-11 16:25:38 -0400

    Revert "tweaked level 5 dialogue"
    This reverts commit b9886697bf62a6db50dc380b418ce884cbc55bab.

--- Cosquest/Assets/Scripts/SceneScripts/Level5.cs
+++ Cosquest/Assets/Scripts/SceneScripts/Level5.cs
@@ -14,9 +14,12 @@ public class Level5 : MonoBehaviour
     [SerializeField] private Image imageHolder;
     [SerializeField] private DialogueManager dm;
 
+    [Header("2nd round box")]
+    [SerializeField] private Text textHolder2;
 
     [Header("Characters")]
     [SerializeField] private PlayerController player;
+    [SerializeField] private Animator dino;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,6 +36,15 @@ public class Level5 : MonoBehaviour
 
         yield return new WaitUntil(DialogueOver);
 
+        dino.SetBool("Visible", true);
+
+        yield return new WaitForSeconds(1.5f);
+
+        dm.dialogueSequence(imageHolder, delay, delayBetweenLines, textHolder2, nameHolder);
+
+        yield return new WaitUntil(DialogueOver);
+
+        dino.SetBool("Visible", false);
     }
 
     bool DialogueOver()
