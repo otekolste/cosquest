b9886697bf62a6db50dc380b418ce884cbc55bab
oliviatek <58699635+oliviatek@users.noreply.github.com>
2023-04-11 10:20:55 -0400

    tweaked level 5 dialogue

--- Cosquest/Assets/Scripts/SceneScripts/Level5.cs
+++ Cosquest/Assets/Scripts/SceneScripts/Level5.cs
@@ -14,12 +14,9 @@ public class Level5 : MonoBehaviour
     [SerializeField] private Image imageHolder;
     [SerializeField] private DialogueManager dm;
 
-    [Header("2nd round box")]
-    [SerializeField] private Text textHolder2;
 
     [Header("Characters")]
     [SerializeField] private PlayerController player;
-    [SerializeField] private Animator dino;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,15 +33,6 @@ public class Level5 : MonoBehaviour
 
         yield return new WaitUntil(DialogueOver);
 
-        dino.SetBool("Visible", true);
-
-        yield return new WaitForSeconds(1.5f);
-
-        dm.dialogueSequence(imageHolder, delay, delayBetweenLines, textHolder2, nameHolder);
-
-        yield return new WaitUntil(DialogueOver);
-
-        dino.SetBool("Visible", false);
     }
 
     bool DialogueOver()
