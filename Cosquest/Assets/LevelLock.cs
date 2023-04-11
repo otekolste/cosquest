801f6f7f1529948664b2c8bdde47750fcbdcc629
oliviatek <58699635+oliviatek@users.noreply.github.com>
2023-04-10 22:03:22 -0400

    reset level1 wanderer position, fixed bug of level screen not showing up in level 3

--- Cosquest/Assets/LevelLock.cs
+++ Cosquest/Assets/LevelLock.cs
@@ -15,7 +15,7 @@ public class LevelLock : MonoBehaviour
 
         for(int i = 0; i < lvlButtons.Length; i++)
         {
-            if (i + 1 > levelAt)
+            if (i +2> levelAt)
                 lvlButtons[i].interactable = false;
         }
         
